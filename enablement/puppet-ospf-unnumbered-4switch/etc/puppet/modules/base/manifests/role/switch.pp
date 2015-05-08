dcda0a29ea7fb898ddc777215ca5bcd7d6540c66
Sean Cavanaugh <sean@cumulusnetworks.com>
2015-05-08 09:39:12 -0400

    remove

--- enablement/puppet-ospf-unnumbered-4switch/etc/puppet/modules/base/manifests/role/switch.pp
+++
@@ -1,9 +0,0 @@
-class base::role::switch {
-include stdlib::stages
-  class { 'base::license':
-    stage => 'setup',
-  }
-
-  include base::interfaces,
-    base::ntpclient
-}
--- enablement/puppet-ospf-unnumbered-4switch/etc/puppet/modules/ospfunnum/manifests/role/switchbase.pp
+++
@@ -1,5 +0,0 @@
-class ospfunnum::role::switchbase {
-    include base::role::switch
-    include base::ptm
-    include ospfunnum::quagga
-}
